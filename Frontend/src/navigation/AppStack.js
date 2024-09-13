7c37c2a7c57827dd4814a06ce5af171e5a29b9e6
Aceek <linhardtilan@gmail.com>
2024-09-13 14:20:27 +0700

    add bottom bar menu

--- Frontend/src/navigation/AppStack.js
+++ Frontend/src/navigation/AppStack.js
@@ -1,18 +1,44 @@
 import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from '../screens/Main/HomeScreen';
 import ProfileScreen from '../screens/Main/ProfileScreen';
 import SettingsScreen from '../screens/Main/SettingsScreen';
 
-const Stack = createNativeStackNavigator();
+const HomeStack = createNativeStackNavigator();
+const ProfileStack = createNativeStackNavigator();
+const SettingsStack = createNativeStackNavigator();
+
+const HomeStackNavigator = () => (
+  <HomeStack.Navigator screenOptions={{headerShown: false}}>
+    <HomeStack.Screen name="HomeStackScreen" component={HomeScreen} />
+  </HomeStack.Navigator>
+);
+
+const ProfileStackNavigator = () => (
+  <ProfileStack.Navigator screenOptions={{headerShown: false}}>
+    <ProfileStack.Screen name="ProfileStackScreen" component={ProfileScreen} />
+  </ProfileStack.Navigator>
+);
+
+const SettingsStackNavigator = () => (
+  <SettingsStack.Navigator screenOptions={{headerShown: false}}>
+    <SettingsStack.Screen
+      name="SettingsStackScreen"
+      component={SettingsScreen}
+    />
+  </SettingsStack.Navigator>
+);
+
+const Tab = createBottomTabNavigator();
 
 const AppStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Profile" component={ProfileScreen} />
-      <Stack.Screen name="Settings" component={SettingsScreen} />
-    </Stack.Navigator>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeStackNavigator} />
+      <Tab.Screen name="Profile" component={ProfileStackNavigator} />
+      <Tab.Screen name="Settings" component={SettingsStackNavigator} />
+    </Tab.Navigator>
   );
 };
 
